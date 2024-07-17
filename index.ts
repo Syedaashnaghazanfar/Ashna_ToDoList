b1b2e47f10d8c6c464d9dad9bc361d0cf284d0f6
Syedaashnaghazanfar <160127890+Syedaashnaghazanfar@users.noreply.github.com>
2024-07-17 16:25:59 +0500

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,45 @@
+#! usr/bin/env node
+import inquirer from "inquirer";
+import chalk from "chalk";
+
+console.log(chalk.bgBlueBright("Hey there! What do you want to do from your list today?"))
+
+let toDoList: string[] = ["Laundry","Cooking","Essentials Shopping","Skincare","Haircare","Dusting House"];
+async function myTodoList(toDoList : string[]) {
+let message1 = await inquirer.prompt([
+    {
+        name :"operationOnlist",
+        type:"list",
+        message:"Please select any one of the following operations",
+        choices:[{value:'Add'},{value:"Clear list"},{value:"view list"},{value:"Exit"}]
+    }    
+    ])
+
+    if(message1.operationOnlist === "Add"){
+        let add = await inquirer.prompt([
+            {
+                name : "Additiononlist",
+                type : "input",
+                message :"Enter the item you want to add in you list"
+            }
+        ])
+     toDoList.push(add.Additiononlist);
+        console.log(chalk.blue(`Item ${add.Additiononlist} added to your list`));
+        console.log(toDoList);
+    }
+    else if (message1.operationOnlist === "Clear list"){
+        toDoList.splice(0,toDoList.length);
+        console.log(chalk.red("Your todo list cleared!"));
+        console.log(toDoList);
+    }
+    else if (message1.operationOnlist === "View list"){
+        console.log(chalk.bgBlueBright("here are you list items"));
+        console.log(toDoList);
+    }
+    else if (message1.operationOnlist === "Exit"){
+        console.log(chalk.red("You have exited your list!"))
+    }
+
+}
+myTodoList(toDoList);
+
